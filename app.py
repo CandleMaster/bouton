bf0c80ff6d7b631099daf2a9c2131691a6b3be36
CandleMaster <djiangfinance@yahoo.com>
2021-12-06 15:21:54 +1100

    tweaked index

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, send_file
 from processing import extract_to_doc
 import zipfile
 import os
-# from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
@@ -13,10 +12,10 @@ app.config['UPLOAD_PATH'] = 'uploads'
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        url = request.form["url"]
         file = request.files['file']
         file_like_object = file.stream._file  
         # translation_option = request.form["translation_option"]
-        url = request.form["url"]
         response = extract_to_doc(file_like_object,url)
         return response
     return render_template('index.html')
